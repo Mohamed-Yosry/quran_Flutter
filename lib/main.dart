b3b02530ce7e11506f807c817610c1ba53b8c480
zyadamin <66642836+zyadamin@users.noreply.github.com>
2021-08-10 07:16:59 -0700

    splsh screenn

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:quran_flutter/splashPage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[700],
+        title: Text("Title of the app"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Home page"),
+        ),
+      ),
     );
   }
 }
--- lib/splashPage.dart
+++ lib/splashPage.dart
@@ -1,1 +1,46 @@
-//here
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(243, 243, 243, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo here
+            Image.asset(
+              'assets/logo2.png',
+              height: 120,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
