566df7613bffdd1d3092a03355eb503c8a8999f6
Manar Atef <ma0452662@gmail.com>
2021-08-11 16:37:24 -0700

    create feature tasbeeh

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:quran_flutter/chapletPage.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _itemCount = 0;
+  @override
+  Widget build(BuildContext context) {
+    return   new Stack(
+        children:[
+          new Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    "assets/bg3@2x.png"),
+                fit: BoxFit.fill,
+
+
+
+              ),
+            ),
+          ),
+          new Column(
+            children: [
+              new Container(
+                padding: EdgeInsets.only(top:30),
+                child: new Text("إسلامي",style: TextStyle(fontSize: 25, color: Colors.black,decorationStyle:TextDecorationStyle.dashed ), textAlign: TextAlign.center,),
+                alignment: Alignment.center,
+              ),
+              new Container(
+                height: 100.0,
+                width: 80.0,
+
+                decoration: BoxDecoration(
+
+                  image: DecorationImage(
+                    image: AssetImage('assets/head of seb7a@2x.png'),
+                  ),
+
+                ),
+              ),
+              new Container(
+                height: 200.0,
+                width: 200.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/body of seb7a@3x.png'),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+              ),
+              new Container(
+                padding: EdgeInsets.only(top:55),
+                child: new Text("عدد التسبيحات",style: TextStyle(fontSize: 20, color: Colors.black), textAlign: TextAlign.center,),
+
+              ),
+
+
+
+            ],
+
+          )
+        ]
     );
   }
 }
