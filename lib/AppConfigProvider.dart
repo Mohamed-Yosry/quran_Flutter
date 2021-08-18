948b8cf4a649c0b988a94cd27948f22197846836
zyadamin <66642836+zyadamin@users.noreply.github.com>
2021-08-17 20:14:18 -0700

    save state of language and theme

--- lib/AppConfigProvider.dart
+++ lib/AppConfigProvider.dart
@@ -1,27 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppConfigProvider extends ChangeNotifier{
   String locale="ar";
-
-  void cahngeLanguage(String language)
-  async{
-    if(language==locale)
-      return;
-
-    locale=language;
-    notifyListeners();
-  }
   ThemeMode themeMode = ThemeMode.light;
 
+  final String Theme = "theme";
+  final String Lang = "language";
 
-  void toggleTheme(){
+  late SharedPreferences _prefs;
+  late bool _darkTheme=false;
+
+  AppConfigProvider() {
+    _loadFromPrefs();
+  }
+
+
+  bool isDarkModeEnable(){
+    return themeMode == ThemeMode.dark;
+  }
+
+
+  toggleTheme() {
     if(themeMode == ThemeMode.light)
       themeMode = ThemeMode.dark;
     else
       themeMode = ThemeMode.light;
     notifyListeners();
+
+    _darkTheme = !_darkTheme;
+    _saveToPrefs();
+    notifyListeners();
   }
-  bool isDarkModeEnable(){
-    return themeMode == ThemeMode.dark;
+
+
+
+
+  _initPrefs() async {
+    if(true)
+      _prefs = await SharedPreferences.getInstance();
+  }
+
+  _loadFromPrefs() async {
+    await _initPrefs();
+    _darkTheme = _prefs.getBool(Theme) ?? false;
+      locale=_prefs.getString(Lang) ?? "ar";
+
+    if(_darkTheme){
+     themeMode = ThemeMode.dark;
+    }
+    else{
+
+      themeMode = ThemeMode.light;
+    }
+
+
+    notifyListeners();
+  }
+
+
+  _saveToPrefs()async {
+    await _initPrefs();
+    _prefs.setString(Lang, locale);
+    _prefs.setBool(Theme, _darkTheme);
+  }
+
+
+  void cahngeLanguage(String language) async{
+    if(language==locale)
+      return;
+
+    locale=language;
+    _saveToPrefs();
+    notifyListeners();
   }
 }
